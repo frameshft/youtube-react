681f4f27e74ed6ed547c9988e41d2e9f95789ae3
frameshft <adiletkadyrov@gmail.com>
2017-06-22 17:14:42 +0600

    Add search

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -5,13 +5,15 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: 'Monster Hunter'};
+    this.state = { term: ''};
 
     this.onInputChange = this.onInputChange.bind(this);
   }
 
   onInputChange(event) {
-    this.setState({ term: event.target.value });
+    const term = event.target.value;
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
   }
 
   render() {
--- src/index.js
+++ src/index.js
@@ -18,17 +18,22 @@ class App extends Component {
       selectedVideo: null,
     };
 
-    YTSearch({key: API_KEY, term: 'monster hunter'}, (videos) => {
+    this.videoSearch('Monster Hunter');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
        this.setState({
          videos,
          selectedVideo: videos[0],
        });
      });
   }
+
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList onVideoSelect={ selectedVideo => this.setState({selectedVideo}) } videos={this.state.videos} />
       </div >
