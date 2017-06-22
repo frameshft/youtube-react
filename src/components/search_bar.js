028713cc8d473f39a2f7b1e0ece5836937fb2afd
frameshft <adiletkadyrov@gmail.com>
2017-06-22 16:32:24 +0600

    Add video list

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { term: 'Starting value'};
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
   render() {
-    return (<input />);
+    return (
+      <div>
+        <input value={this.state.term} onChange={this.onInputChange}/>
+        Value of the input: {this.state.term}
+      </div>
+    );
   }
 }
 
